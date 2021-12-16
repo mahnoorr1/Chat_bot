889d5c5715e94541dd25a848b288b0a0c8f5786e
Mahnoor Hashmi <91974252+mahnoorr1@users.noreply.github.com>
2021-12-16 17:47:05 +0500

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -1,17 +1,15 @@
-class user{
-  String name;
-  String pass;
-  user(this.name, this.pass);
-  String getName(){
-    return this.name;
-  }
-  void setName(String name){
-    this.name = name;
-  }
-  String getPass(){
-    return this.pass;
-  }
-  void setPass(String pass){
-    this.pass = pass;
-  }
+class User {
+  final String name;
+  final String email;
+
+  User(this.name, this.email);
+
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        email = json['email'];
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+      };
 }
